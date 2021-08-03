2aac9e4a7d1ba9bf9fe108d89d41037380b78b28
jisulog <jisu.rok@gmail.com>
2021-08-03 11:40:41 +0900

    Add update, delete

--- cloudmarket/market/views/post_views.py
+++ cloudmarket/market/views/post_views.py
@@ -1,38 +1,54 @@
-from django.shortcuts import redirect, render
-from django.views.generic import ListView, DetailView, CreateView
+from django.shortcuts import get_object_or_404
+from django.urls.base import reverse
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from market.models import Post
 from market.forms import PostForm
 from django.utils import timezone
+from django.urls import reverse_lazy
 
 # Create your views here.
 # CBV(Class Based View) 사용하기
 
 class PostList(ListView):
-    paginate_by = 10
+    paginate_by = 6
     def get_queryset(self):
         return Post.objects.order_by('-create_date')
 
 class PostCreate(CreateView):
-    template_name = 'market/post_create.html'
     form_class = PostForm
+    template_name = 'market/post_create.html'
+    success_url = reverse_lazy('market:postdetail')
 
-    def post(self, request):
-        form = PostForm(request.POST, request.FILES)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.create_date = timezone.now()
-            post.save()
-            return redirect('market:postlist')
-        else:
-            form = PostForm()
-        return render(request, 'market/post_create.html', {'form':form})
+    def form_valid(self, form):
+        post = form.save(False)
+        post.create_date = timezone.now()
+        post.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('market:postdetail', kwargs={'pk':self.object.pk})
 
 class PostDetail(DetailView):
     model = Post
-    queryset = Post.objects.all()
 
-class PostUpdate(ListView):
-    pass
+class PostUpdate(UpdateView):
+    form_class = PostForm
+    template_name = 'market/post_update.html'
+    success_url = reverse_lazy('market:postdetail')
 
-class PostDelete(ListView):
-    pass
+    def get_object(self):
+        post = get_object_or_404(Post, pk=self.kwargs['pk'])
+        post.modify_date = timezone.now()
+        post.save()
+        return post
+
+    def get_success_url(self):
+        return reverse_lazy('market:postdetail', kwargs={'pk':self.object.pk})
+
+
+class PostDelete(DeleteView):
+    model = Post
+    success_url = '/'
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
