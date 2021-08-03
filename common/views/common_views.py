655ab1db1b781f03bbb34bd3444c8ddd387918d9
seokhwanchoi <bmsk20@gmail.com>
2021-08-03 18:28:06 +0900

    1. post_update.html/ 2. hanbipost update/ 3.views directory make/ 4. common_views.py make/ 5. class based view

---
+++ common/views/common_views.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from common.forms import UserForm
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+class CreateUserView(CreateView):  # 제네릭의 CreateView는 폼하고 연결돼서, 혹은 모델하고 연결돼서 새로운 데이터를 넣을 때 사용.
+    template_name = 'common/signup.html'     # 회원가입 할 때 띄울 폼 템플릿
+    form_class = UserForm
+    success_url = reverse_lazy('market:postlist') # 성공하면 어디로 갈지, create_user_done은 url name
+    # 여기서 reverse가 아닌 reverse_lazy를 사용하는 이유: 제네릭뷰 같은경우 타이밍 문제 때문에 reverse_lazy를 사용해야함
+
+
+'''
+def signup(request):
+    """
+    계정생성
+    """
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)  # 사용자 인증
+            login(request, user)  # 로그인
+            return redirect('market:postlist')
+    else:
+        form = UserForm()
+    return render(request, 'common/signup.html', {'form': form})
+'''
