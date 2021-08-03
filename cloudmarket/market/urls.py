efbc7926e6adbd27ac9d242e01bfa64b9b5594f5
seokhwanchoi <bmsk20@gmail.com>
2021-08-03 13:23:53 +0900

    login logout signup

--- cloudmarket/market/urls.py
+++ cloudmarket/market/urls.py
@@ -1,18 +1,4 @@
-"""project URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
 from market import views
 
@@ -25,5 +11,8 @@ app_name = 'market'
 
 
 urlpatterns = [
-     path('', views.PostList.as_view(), name="post"),
+     path('',views.index, name='index'),
+     path('<int:post_id>/', views.detail, name='detail'),
+     path('comment/create/<int:post_id>/', views.comment_create, name='comment_create'),
+    #path('', views.PostList.as_view(), name="post"),
 ]
